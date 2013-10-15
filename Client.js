82b8b21e2676b0d68b6bcccdb934455a0dfae69f
Samantha Quek <samanthaquek@outlook.com>
2013-10-15 11:14:46 +0800

    box2d replaces physijs

--- Client.js
+++ Client.js
@@ -358,129 +358,181 @@ function Client(){
 		
 		$(document).unbind();
 		//TODO start binding onto gaming keys
-		
+		$('#contentHTML').append('<canvas id="canvas" width="600" height="400" style="background-color:#333333;" ></canvas>');
+		init();
 		//window.innerWidth / window.innerHeight
-
 		//TODO rendering init
-		if (window.WebGLRenderingContext){
-			renderer = new THREE.WebGLRenderer(); 
-			console.log("webgl");
-		}else{
-			renderer = new THREE.CanvasRenderer();
-			console.log("canvas renderer");
-		}
-		renderer.setSize( $(window).width(), $(window).height());
-		//Enable shadows on renderer
-		renderer.shadowMapEnabled = true;
-		renderer.shadowMapSoft = true;
-		$('#contentHTML').append(renderer.domElement);
-		
-		//scene = new THREE.Scene();
-		scene = new Physijs.Scene;
-		scene.setGravity(new THREE.Vector3( 0, -50, 0 ));
+		function init() {
+			console.log("entered init");
+         var   b2Vec2 = Box2D.Common.Math.b2Vec2
+            ,  b2AABB = Box2D.Collision.b2AABB
+         	,	b2BodyDef = Box2D.Dynamics.b2BodyDef
+         	,	b2Body = Box2D.Dynamics.b2Body
+         	,	b2FixtureDef = Box2D.Dynamics.b2FixtureDef
+         	,	b2Fixture = Box2D.Dynamics.b2Fixture
+         	,	b2World = Box2D.Dynamics.b2World
+         	,	b2MassData = Box2D.Collision.Shapes.b2MassData
+         	,	b2PolygonShape = Box2D.Collision.Shapes.b2PolygonShape
+         	,	b2CircleShape = Box2D.Collision.Shapes.b2CircleShape
+         	,	b2DebugDraw = Box2D.Dynamics.b2DebugDraw
+            ,  b2MouseJointDef =  Box2D.Dynamics.Joints.b2MouseJointDef
+            ;
+         
+         var world = new b2World(
+               new b2Vec2(0, 0)    //gravity is zero since top-down
+            ,  true                 //allow sleep
+         );
+         
+         var fixDef = new b2FixtureDef;
+         fixDef.density = 1.0;
+         fixDef.friction = 0.5;
+         fixDef.restitution = 0.2;
+         
+         var bodyDef = new b2BodyDef;
+         
+         //create ground
+         bodyDef.type = b2Body.b2_staticBody;
+         fixDef.shape = new b2PolygonShape;
+         fixDef.shape.SetAsBox(20, 2);
+         bodyDef.position.Set(10, 400 / 30 + 1.8);
+         world.CreateBody(bodyDef).CreateFixture(fixDef);
+         bodyDef.position.Set(10, -1.8);
+         world.CreateBody(bodyDef).CreateFixture(fixDef);
+         fixDef.shape.SetAsBox(2, 14);
+         bodyDef.position.Set(-1.8, 13);
+         world.CreateBody(bodyDef).CreateFixture(fixDef);
+         bodyDef.position.Set(21.8, 13);
+         world.CreateBody(bodyDef).CreateFixture(fixDef);
+         
+         
+         //create some objects
+         bodyDef.type = b2Body.b2_dynamicBody;
+         for(var i = 0; i < 10; ++i) {
+            if(Math.random() > 0.5) {
+               fixDef.shape = new b2PolygonShape;
+               fixDef.shape.SetAsBox(
+                     Math.random() + 0.1 //half width
+                  ,  Math.random() + 0.1 //half height
+               );
+            } else {
+               fixDef.shape = new b2CircleShape(
+                  Math.random() + 0.1 //radius
+               );
+            }
+            bodyDef.position.x = Math.random() * 10;
+            bodyDef.position.y = Math.random() * 10;
+            world.CreateBody(bodyDef).CreateFixture(fixDef);
+         }
+         
+         //setup debug draw
+         var debugDraw = new b2DebugDraw();
+			debugDraw.SetSprite(document.getElementById("canvas").getContext("2d"));
+			debugDraw.SetDrawScale(30.0);
+			debugDraw.SetFillAlpha(0.5);
+			debugDraw.SetLineThickness(1.0);
+			debugDraw.SetFlags(b2DebugDraw.e_shapeBit | b2DebugDraw.e_jointBit);
+			world.SetDebugDraw(debugDraw);
+         
+         window.setInterval(update, 1000 / 60);
+         
+         //mouse
+         
+         var mouseX, mouseY, mousePVec, isMouseDown, selectedBody, mouseJoint;
+         var canvasPosition = getElementPosition(document.getElementById("canvas"));
+         
+         document.addEventListener("mousedown", function(e) {
+            isMouseDown = true;
+            handleMouseMove(e);
+            document.addEventListener("mousemove", handleMouseMove, true);
+         }, true);
+         
+         document.addEventListener("mouseup", function() {
+            document.removeEventListener("mousemove", handleMouseMove, true);
+            isMouseDown = false;
+            mouseX = undefined;
+            mouseY = undefined;
+         }, true);
+         
+         function handleMouseMove(e) {
+            mouseX = (e.clientX - canvasPosition.x) / 30;
+            mouseY = (e.clientY - canvasPosition.y) / 30;
+         };
+         
+         function getBodyAtMouse() {
+            mousePVec = new b2Vec2(mouseX, mouseY);
+            var aabb = new b2AABB();
+            aabb.lowerBound.Set(mouseX - 0.001, mouseY - 0.001);
+            aabb.upperBound.Set(mouseX + 0.001, mouseY + 0.001);
+            
+            // Query the world for overlapping shapes.
 
-		scene.addEventListener(
-			'update',
-			function() {
-				scene.simulate( undefined, 1 );
-			}
-		);
-		console.log("created scene & set gravity");
-		//PerspectiveCamera( angle, aspect ratio, near, far )
-		camera = new THREE.PerspectiveCamera( 20,  $(window).width()/$(window).height() , 0.1 , 10000 );
-		/*camera.position.y = -GameConstants.PLATFORM_WIDTH-GameConstants.PLATFORM_HEIGHT-100; //tilt camera downwards
-		camera.position.x = GameConstants.PLATFORM_WIDTH+GameConstants.PLATFORM_HEIGHT;
-		camera.position.z = GameConstants.PLATFORM_WIDTH+GameConstants.PLATFORM_HEIGHT;*/
-		camera.position.set(0, 200, 300);
-		camera.lookAt( scene.position ); // the origin
-		//camera.rotation.z = -camera.rotation.z; //set back to horizontal plane view
-		
-		scene.add(camera);
-		console.log("added camera");
-		//Create lighting on top
-		var light = new THREE.DirectionalLight(0xFFFFFF);
-		light.position.set(0,200,200);
-		light.castShadow = true;
-		scene.add(light);
-		
-		var sphere, sphere2, material;
-		var sphere_geometry = new THREE.SphereGeometry( 5, 15, 15 );
-		material = Physijs.createMaterial(
-			new THREE.MeshLambertMaterial({ map: THREE.ImageUtils.loadTexture( 'images/plywood.jpg' ) }),
-			.6, // medium friction
-			.3 // low restitution
-			
-		);
-		material.map.wrapS = material.map.wrapT = THREE.RepeatWrapping;
-		material.map.repeat.set( .5, .5 );
-		
-		sphere = new Physijs.SphereMesh(
-			sphere_geometry,
-			material
-		);
-				
-		sphere.position.set(
-			Math.random() * 15 - 7.5,
-			50,
-			Math.random() * 15 - 7.5
-		);
-		
-		sphere.castShadow = true;
-		sphere.receiveShadow = true;
-		scene.add( sphere );
+            selectedBody = null;
+            world.QueryAABB(getBodyCB, aabb);
+            return selectedBody;
+         }
 
-		sphere2 = new Physijs.SphereMesh(
-			sphere_geometry,
-			material
-		);
-				
-		sphere2.position.set(
-			0,
-			10,
-			0
-		);		
+         function getBodyCB(fixture) {
+            if(fixture.GetBody().GetType() != b2Body.b2_staticBody) {
+               if(fixture.GetShape().TestPoint(fixture.GetBody().GetTransform(), mousePVec)) {
+                  selectedBody = fixture.GetBody();
+                  return false;
+               }
+            }
+            return true;
+         }
+         
+         //update
+         
+         function update() {
+         
+            if(isMouseDown && (!mouseJoint)) {
+               var body = getBodyAtMouse();
+               if(body) {
+                  var md = new b2MouseJointDef();
+                  md.bodyA = world.GetGroundBody();
+                  md.bodyB = body;
+                  md.target.Set(mouseX, mouseY);
+                  md.collideConnected = true;
+                  md.maxForce = 300.0 * body.GetMass();
+                  mouseJoint = world.CreateJoint(md);
+                  body.SetAwake(true);
+               }
+            }
+            
+            if(mouseJoint) {
+               if(isMouseDown) {
+                  mouseJoint.SetTarget(new b2Vec2(mouseX, mouseY));
+               } else {
+                  world.DestroyJoint(mouseJoint);
+                  mouseJoint = null;
+               }
+            }
+         
+            world.Step(1 / 60, 10, 10);
+            world.DrawDebugData();
+            world.ClearForces();
+         };
+         //http://js-tut.aardon.de/js-tut/tutorial/position.html
+         function getElementPosition(element) {
+            var elem=element, tagname="", x=0, y=0;
+           
+            while((typeof(elem) == "object") && (typeof(elem.tagName) != "undefined")) {
+               y += elem.offsetTop;
+               x += elem.offsetLeft;
+               tagname = elem.tagName.toUpperCase();
 
-		
-		sphere2.castShadow = true;
-		sphere2.receiveShadow = true;
-		scene.add( sphere2 );
+               if(tagname == "BODY")
+                  elem=0;
 
-		//CubeGeometry(width, height, depth, widthSegments, heightSegments, depthSegments)
+               if(typeof(elem) == "object") {
+                  if(typeof(elem.offsetParent) == "object")
+                     elem = elem.offsetParent;
+               }
+            }
 
-		// Ground
-		var ground_material = Physijs.createMaterial(
-			new THREE.MeshLambertMaterial({ map: THREE.ImageUtils.loadTexture( 'images/rocks.jpg' ) }),
-			.8, // high friction
-			.3 // low restitution
-		);
-		ground_material.map.wrapS = ground_material.map.wrapT = THREE.RepeatWrapping;
-		ground_material.map.repeat.set( 3, 3 );
-		
-		var ground = new Physijs.BoxMesh(
-			new THREE.CubeGeometry(100, 1, 100),
-			ground_material,
-			0 // mass
-		);
-		ground.receiveShadow = true;
-
-		scene.add( ground );
-		console.log("created ground");
-
-		
-/*		var playField = new THREE.Mesh (new THREE.CubeGeometry ( GameConstants.PLATFORM_WIDTH, GameConstants.PLATFORM_HEIGHT, -GameConstants.PLATFORM_DEPTH ), material2);
-		playField.receiveShadow = true;
-		scene.add( playField );*/
-		scene.simulate();
-		animateGame();
-		
-	}
-	
-	var animateGame = function(){
-		requestAnimationFrame( animateGame );
-		scene.simulate();
-		//TODO all the reaction to keys and logic
-		renderer.render( scene, camera );
-		console.log("render");
+            return {x: x, y: y};
+         }
+     	}
 	}
 	
 	var unloadGame = function(){
